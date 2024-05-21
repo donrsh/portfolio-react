69553d2eafc5a58d23305f52c46f909b7a701eb1
Don.Hsieh <don.shieh@gmail.com>
2024-05-21 22:56:30 +0800

    init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,25 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>React Portfolio</h1>
+      <p>
+        This site serves as a portfolio of my react skill.
+        <br />
+        It is based on Vite, and also an app under my{" "}
+        <a href="https://github.com/donrsh/web-apps-workspace/tree/main">
+          web apps workspace
+        </a>{" "}
+        project .
+      </p>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      port: Number(env.VITE_DEV_PORT),
+      open: true,
+    },
+    plugins: [
+      remix({
+        ssr: false,
+      }),
+      tsconfigPaths(),
+    ],
+  };
+});
