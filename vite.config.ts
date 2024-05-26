3bcdf0add09f3818a2d1e8ee62d9746959ec8d4c
Don.Hsieh <don.shieh@gmail.com>
2024-05-26 09:45:28 +0800

    config default path to prevent vite dev crush

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const base = process.env.BASE_URL ?? undefined;
+const base = process.env.BASE_URL ?? "/";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
