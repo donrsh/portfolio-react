239bc4c1ec280b6e6a11a1f104fc843beac29cb3
Don.Hsieh <don.shieh@gmail.com>
2024-06-04 22:57:00 +0800

    add manifest for pwa

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="manifest"
+          href={`${import.meta.env.BASE_URL}app.webmanifest`}
+        ></link>
         <Meta />
         <Links />
       </head>
