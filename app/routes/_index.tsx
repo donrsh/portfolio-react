0ea0c17af9ec30f9f7ce0178fbde4130f4671718
Don Hsieh <don.shieh@gmail.com>
2024-09-16 17:56:37 +0800

    add shadcn components and adopt in homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,15 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link as RemixLink } from "@remix-run/react";
 import { type ComponentProps } from "react";
+import { Separator } from "~/lib/ui/separator";
+import { Typography } from "~/lib/ui/typogrphy";
+import { Link } from "~/lib/ui/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/lib/ui/accordion";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Donrsh | React Portfolio" }];
@@ -8,74 +17,110 @@ export const meta: MetaFunction = () => {
 
 const ExternalLink = (props: ComponentProps<"a">) => {
   return (
-    <a {...props} target="_blank" rel="noreferrer">
+    <Link {...props} target="_blank" rel="noreferrer">
       {props.children!}
-    </a>
+    </Link>
   );
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <p>
+    <div className="p-4 ">
+      <Typography variant="muted" className="leading-relaxed mb-2">
         This site serves as a portfolio of my{" "}
-        <ExternalLink href="https://react.dev/">React</ExternalLink> skill.
-        <br />
-        It is based on{" "}
-        <ExternalLink href="https://vitejs.dev/">Vite</ExternalLink> and{" "}
-        <ExternalLink href="https://remix.run/">Remix</ExternalLink>, and also
-        an app under my{" "}
+        <ExternalLink href="https://react.dev/">React</ExternalLink> skill. It
+        is based on <ExternalLink href="https://vitejs.dev/">Vite</ExternalLink>{" "}
+        and <ExternalLink href="https://remix.run/">Remix</ExternalLink>, and
+        also an app under my{" "}
         <ExternalLink href="https://github.com/donrsh/web-apps-workspace/tree/main">
           web apps workspace
         </ExternalLink>{" "}
         project.
         <br />
+      </Typography>
+
+      <Typography variant="muted" className="leading-relaxed">
         This site is a{" "}
         <ExternalLink href="https://web.dev/articles/what-are-pwas?hl=zh-tw">
           Progress Web App (PWA)
         </ExternalLink>{" "}
         as well. You can install it to your mobile devices.
-      </p>
+      </Typography>
 
-      <h2>Demos</h2>
-      <ul>
-        <li>
-          <Link to="/demo/useToggler">
-            react hook: <code>useToggler</code>
-          </Link>
-        </li>
-        <li>
-          <Link to="/demo/useTmpAuthorization-zustand">
-            react hook: <code>useTmpAuthorization</code> (impl w/ zustand)
-          </Link>
-        </li>
-        <li>
-          <Link to="/demo/useAnimateOnUpdate">
-            react hook: <code>useAnimateOnUpdate</code>
-          </Link>
-        </li>
-        <li>
-          <Link to="/demo/useHorizontalScroll">
-            react hook: <code>useHorizontalScroll</code>
-          </Link>
-        </li>
-      </ul>
+      <Separator className="my-4" />
 
-      <h2>Applets</h2>
-      <ul>
-        <li>
-          <Link to="/applet/fullscreen-emoji">Full Screen Emoji</Link>
-        </li>
-      </ul>
+      <Accordion type="multiple" defaultValue={["demos", "applets", "ux"]}>
+        <AccordionItem value="demos">
+          <AccordionTrigger className="text-lg">Demos</AccordionTrigger>
+          <AccordionContent>
+            <Typography variant="ul">
+              <li>
+                react hook:{" "}
+                <Link asChild>
+                  <RemixLink to="/demo/useToggler">
+                    <code>useToggler</code>
+                  </RemixLink>
+                </Link>
+              </li>
+              <li>
+                react hook:{" "}
+                <Link asChild>
+                  <RemixLink to="/demo/useTmpAuthorization-zustand">
+                    <code>useTmpAuthorization</code>
+                  </RemixLink>
+                </Link>{" "}
+                (impl w/ zustand)
+              </li>
+              <li>
+                react hook:{" "}
+                <Link asChild>
+                  <RemixLink to="/demo/useAnimateOnUpdate">
+                    <code>useAnimateOnUpdate</code>
+                  </RemixLink>
+                </Link>
+              </li>
+              <li>
+                react hook:{" "}
+                <Link asChild>
+                  <RemixLink to="/demo/useHorizontalScroll">
+                    <code>useHorizontalScroll</code>
+                  </RemixLink>
+                </Link>
+              </li>
+            </Typography>
+          </AccordionContent>
+        </AccordionItem>
 
-      <h2>UX</h2>
-      <ul>
-        <li>
-          <Link to="/ux/auto-focus-when-scrolling">
-            Auto Focus when Scrolling
-          </Link>
-        </li>
-      </ul>
+        <AccordionItem value="applets">
+          <AccordionTrigger className="text-lg">Applets</AccordionTrigger>
+          <AccordionContent>
+            <Typography variant="ul">
+              <li>
+                <Link asChild>
+                  <RemixLink to="/applet/fullscreen-emoji">
+                    Full Screen Emoji
+                  </RemixLink>
+                </Link>
+              </li>
+            </Typography>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="ux">
+          <AccordionTrigger className="text-lg">UX</AccordionTrigger>
+          <AccordionContent>
+            <Typography variant="ul">
+              <li>
+                <Link asChild>
+                  <RemixLink to="/ux/auto-focus-when-scrolling">
+                    Auto Focus when Scrolling
+                  </RemixLink>
+                </Link>
+              </li>
+            </Typography>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
