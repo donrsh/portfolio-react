e0706f5f4c6e5520574c7db59ab3e51f3aca4810
Don Hsieh <don.shieh@gmail.com>
2024-09-17 10:19:44 +0800

    remove nav bar

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -1,15 +1,9 @@
-import { Link, Outlet } from "@remix-run/react";
-import { Typography } from "~/lib/ui/typogrphy";
+import { Outlet } from "@remix-run/react";
 
 export default function Layout() {
   return (
     <>
-      <div style={{ padding: 16, marginBottom: 8, background: "#dddddd" }}>
-        <Link to="/">Home</Link>
-      </div>
-
       <div className="p-4 -mt-4">
-        <Typography variant="h2">Demo</Typography>
         <div className="py-2">
           <Outlet />
         </div>
