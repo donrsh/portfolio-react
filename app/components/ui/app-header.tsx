037e11e256c42ef6a28718d2b50e14c764abfd51
Don Hsieh <don.shieh@gmail.com>
2024-09-21 23:16:12 +0800

    show home icon instead of title on subpages

--- app/components/ui/app-header.tsx
+++ app/components/ui/app-header.tsx
@@ -1,12 +1,21 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import { ThemeModeToggler } from "~/components/ui/mode-toggler";
 import { Typography } from "~/lib/ui/typogrphy";
+import { HomeIcon } from "lucide-react";
 
 export function AppHeader() {
+  const { pathname } = useLocation();
+
   return (
     <header className="px-4 py-6 flex items-center">
       <Typography variant="h2" className="mr-auto border-b-0 pb-0">
-        <Link to="/">Don's React Portfolio</Link>
+        <Link to="/">
+          {pathname === "/" ? (
+            "Don's React Portfolio"
+          ) : (
+            <HomeIcon className="ml-1" />
+          )}
+        </Link>
       </Typography>
       <ThemeModeToggler />
     </header>
