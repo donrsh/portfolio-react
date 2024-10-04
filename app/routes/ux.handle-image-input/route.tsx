190da4b0bd65d506a9724eda93d605ba78b99542
Don Hsieh <don.shieh@gmail.com>
2024-10-05 01:08:41 +0800

    ux.handle-image-input: multiple input region

--- app/routes/ux.handle-image-input/route.tsx
+++ app/routes/ux.handle-image-input/route.tsx
@@ -1,5 +1,5 @@
 import { createSub } from "@lib-react/utils/createSub";
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import { Button } from "~/lib/ui/button";
 import { Typography } from "~/lib/ui/typogrphy";
 import { Input } from "~/lib/ui/input";
@@ -33,7 +33,56 @@ const useImages = () => {
 };
 
 const Sub = createSub({
-  ImageInput: ({ addImage }: { addImage: (file: File) => void }) => {
+  InputRegion: ({ title }: { title: string }) => {
+    const { images, addImage, removeImage } = useImages();
+    const editorRef = useRef<HTMLDivElement>(null);
+
+    // handle paste event
+    useEffect(() => {
+      const handlePaste = (event: ClipboardEvent) => {
+        event.preventDefault();
+        const item = event.clipboardData?.items?.[0];
+
+        if (!item) return;
+
+        if (item.type.startsWith("image/")) {
+          addImage(item.getAsFile()!);
+        }
+      };
+
+      editorRef.current?.addEventListener("paste", handlePaste);
+
+      return () => editorRef.current?.removeEventListener("paste", handlePaste);
+    }, []);
+
+    return (
+      <div>
+        <div className="flex items-center gap-2 mb-2">
+          <Typography variant="large">{title}</Typography>
+          <span>-</span>
+          <div>
+            Paste images (Ctrl / Cmd + V) or{" "}
+            <Sub.ImageFileInput addImage={addImage} />
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          ref={editorRef}
+          className="border rounded-md p-2 min-h-[300px] overflow-y-auto flex flex-wrap gap-2 focus:ring-2 focus:ring-blue-500"
+        >
+          {images.reverse().map((image) => (
+            <Sub.Image
+              key={image.id}
+              {...image}
+              remove={() => removeImage(image.id)}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  },
+
+  ImageFileInput: ({ addImage }: { addImage: (file: File) => void }) => {
     const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
       const file = event.target.files?.[0];
       if (file) {
@@ -54,15 +103,16 @@ const Sub = createSub({
       </label>
     );
   },
-  Image: ({ dataUrl, remove }: ImageFile & { remove: () => void }) => {
+
+  Image: ({ dataUrl, remove, id }: ImageFile & { remove: () => void }) => {
     return (
-      <div className="relative group border rounded-md overflow-hidden">
-        <img src={dataUrl} className="h-[200px] " />
+      <div className="relative group border rounded-md overflow-hidden h-fit">
+        <img src={dataUrl} className="h-[200px]" />
         <Button
           variant="outline"
           size="icon"
           onClick={remove}
-          className="absolute top-4 right-4 hidden group-hover:inline-flex"
+          className={`absolute top-4 right-4 hidden group-hover:inline-flex`}
         >
           <Trash />
         </Button>
@@ -72,45 +122,11 @@ const Sub = createSub({
 });
 
 export default function Page() {
-  const { images, addImage, removeImage } = useImages();
-
-  useEffect(() => {
-    const handlePaste = (event: ClipboardEvent) => {
-      const item = event.clipboardData?.items?.[0];
-
-      if (!item) return;
-
-      if (item.type.startsWith("image/")) {
-        addImage(item.getAsFile()!);
-      }
-    };
-
-    document.addEventListener("paste", handlePaste);
-
-    return () => document.removeEventListener("paste", handlePaste);
-  }, []);
-
   return (
     <div className="px-4">
-      <div className="flex flex-col items-center">
-        <Typography
-          variant="large"
-          className="flex items-center gap-1 mb-4 text-center"
-        >
-          Paste images (Ctrl/Cmd + V) or <Sub.ImageInput addImage={addImage} />
-        </Typography>
-      </div>
-
-      <hr className="mx-[100px] mb-4" />
-
-      <div className="flex flex-wrap gap-4">
-        {images.reverse().map((image) => (
-          <Sub.Image
-            key={image.id}
-            {...image}
-            remove={() => removeImage(image.id)}
-          />
-        ))}
+      <div className="flex flex-col gap-8">
+        <Sub.InputRegion title="Region 1" />
+        <Sub.InputRegion title="Region 2" />
       </div>
     </div>
   );
